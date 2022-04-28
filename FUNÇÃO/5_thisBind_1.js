b240336540cb1883ef144c980243d2e311c77260
Alan_Morais <alanmoraisrn@gmail.com>
2022-04-27 21:03:08 -0300

    MÉTODOS THIS E BIND EM JS

---
+++ FUNÇÃO/5_thisBind_1.js
@@ -0,0 +1,18 @@
+//THIS E BIND
+
+const pessoa = {
+    saudar: "Olá, bom dia!",
+    falar (){
+        console.log(this.saudar);
+    }
+}
+
+pessoa.falar();
+const falar = pessoa.falar;
+falar(); //CONFLITO DE PARADIGMA: FUNCIONAL E ORIENTAÇÃO A OBJETO
+
+const falatorio = pessoa.falar.bind(pessoa); //BIND É RESPONSÁVEL POR AMARRAR DETERMINADO OBJETO
+falatorio();
+
+const falar2 = pessoa.falar;
+falar2(); //DEMONSTRAR QUE A FUNÇÃO FALAR NÃO FOI ALTERADA PELA CHAMADA DO MÉTODO BIND
